97b7ee834d63b38c51788c07eeb0196c79a390af
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2020-10-26 17:19:33 -0500

    commit 2 - added home, about, and vinyl index paths

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Home')
+
+def about(request):
+    return HttpResponse('About')
+
+def vinyls(request):
+    return HttpResponse('All Vinyls')
